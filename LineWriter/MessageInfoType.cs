1f1404053eadfc29e1327a2e32ca1cf17e62c8eb
Javier Gómez Luzón <javo14795@gmail.com>
2022-12-03 22:42:49 +0100

    Create MessageInfoType.cs

---
+++ LineWriter/MessageInfoType.cs
@@ -0,0 +1,10 @@
+﻿namespace ImageDater.LineWriter
+{
+    public enum MessageInfoType
+    {
+        DONT_UNDERSTAND = 1,
+        DIRECTORY_PATH = 2,
+        IMAGE_PATH = 3,
+        MODE_QUESTION = 4
+    }
+}
