45a0d20113ecaa0d94fb41273ccf11a22c550472
Javier Gómez Luzón <javo14795@gmail.com>
2022-12-04 17:55:38 +0100

    Create MessageDictionary

---
+++ Messager/MessageDictionary.cs
@@ -0,0 +1,23 @@
+﻿namespace ImageDater.Messager
+{
+    public sealed class MessageDictionary
+    {
+        private static MessageDictionary? _instance = null;
+        public static MessageDictionary Instance
+        {
+            get
+            {
+                if(_instance == null)
+                    _instance = new MessageDictionary();
+                return _instance;
+            }
+        }
+        private MessageDictionary() { 
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+        }
+    }
+}
