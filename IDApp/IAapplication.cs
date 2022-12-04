ddc63f8150a9deec759ee25c210144cc4c9ca231
Javier Gómez Luzón <javo14795@gmail.com>
2022-12-04 17:57:49 +0100

    Create IAapplication.cs

---
+++ IDApp/IAapplication.cs
@@ -0,0 +1,21 @@
+﻿using ImageDater.Files;
+using ImageDater.Messager;
+
+namespace ImageDater.IDApp
+{
+    public  class IAapplication
+    {
+        private readonly LineWriter _lineWriter;
+        private readonly FileManager _fileManager;
+
+        public IAapplication()
+        {
+            _lineWriter = new LineWriter();
+            _fileManager = new FileManager();
+        }
+
+        public void run()
+        {
+        }
+    }
+}
